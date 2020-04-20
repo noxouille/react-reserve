559febf4cf23b32bf022a9ec33d3c44626ddcc56
noxouille <fp9821@gmail.com>
2020-04-20 14:00:59 -0400

    add CRUD functionality (Section 6)

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 // must restart server whenever you make changes in next.config
 module.exports = {
   env: {
-    MONGO_SRV: "<insert-mongo-srv>",
+    MONGO_SRV:
+      "mongodb+srv://noxouille:ansiSakurios60@reactreserve-tmkte.mongodb.net/test?retryWrites=true&w=majority",
     JWT_SECRET: "<insert-jwt-secret>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
-  }
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/ferdipratama/image/upload",
+    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>",
+  },
 };
