261b3ca7aeabd526e7587848afd514978d2b79bf
noxouille <fp9821@gmail.com>
2020-04-20 18:51:36 -0400

    Model User, Signup User (8c)

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ module.exports = {
   env: {
     MONGO_SRV:
       "mongodb+srv://noxouille:ansiSakurios60@reactreserve-tmkte.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "thisisarandomstringcanbeanything",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/ferdipratama/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>",
   },
