e3e3b0ddc47d91a39baa5dcb571b099fe65828e1
noxouille <fp9821@gmail.com>
2020-04-21 11:21:45 -0400

    add products to cart (10b)

--- components/Product/ProductSummary.js
+++ components/Product/ProductSummary.js
@@ -1,7 +1,7 @@
 import { Item, Label } from "semantic-ui-react";
-import AddProductToCart from "../Product/AddProductToCart";
+import AddProductToCart from "./AddProductToCart";
 
-function ProductSummary({ name, mediaUrl, _id, price, sku }) {
+function ProductSummary({ name, mediaUrl, _id, price, sku, user }) {
   // return <>ProductSummary</>;
   return (
     <Item.Group>
@@ -14,7 +14,7 @@ function ProductSummary({ name, mediaUrl, _id, price, sku }) {
             <Label>SKU: {sku}</Label>
           </Item.Description>
           <Item.Extra>
-            <AddProductToCart productId={_id} />
+            <AddProductToCart user={user} productId={_id} />
           </Item.Extra>
         </Item.Content>
       </Item>
